96479cfcfaa72e45a12cf68e1a7d73114556f35c
홍종훈 <whdgnszz1@snu.ac.kr>
2025-05-09 12:14:59 +0900

    Feat: 이벤트 전체 조회, 단일 조회 기능 추가

--- src/main/java/com/birthae/be/event/EventService.java
+++ src/main/java/com/birthae/be/event/EventService.java
@@ -1,9 +1,13 @@
 package com.birthae.be.event;
 
+import com.birthae.be.common.exception.BizRuntimeException;
 import com.birthae.be.event.entity.Event;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +21,16 @@ public class EventService {
         newEvent.setDescription(description);
         newEvent.setImageUrl(imageUrl);
         return eventRepository.save(newEvent);
+    }
+
+    public Page<Event> getAllEvents(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+        return eventRepository.findAll(pageable);
+    }
+
+
+    public Event getEventById(Long eventId) {
+        return eventRepository.findById(eventId)
+                .orElseThrow(() -> new BizRuntimeException(String.valueOf(HttpStatus.NOT_FOUND.value()), "이벤트가 존재하지 않습니다."));
     }
 }
